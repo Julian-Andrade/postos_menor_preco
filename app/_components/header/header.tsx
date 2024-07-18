971a911ac41e30068ed72a4269b18603825a13ec
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-07-17 23:06:50 -0300

    feat: add Header component

---
+++ app/_components/header/header.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import NavItem from "./nav-items";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+
+const NAV_ITEMS = [
+  {
+    label: "Página Inicial",
+    href: "/",
+  },
+  {
+    label: "Nossa História",
+    href: "/nossa_historia",
+  },
+  {
+    label: "Produtos",
+    href: "/produtos",
+  },
+  {
+    label: "Rede de Postos",
+    href: "/rede_de_postos",
+  },
+  {
+    label: "Trabalhe Conosco",
+    href: "/rede_de_postos",
+  },
+  {
+    label: "Contato",
+    href: "/contato",
+  },
+];
+
+const Header = () => {
+  return (
+    <div className="bg-primary py-2 shadow-md">
+      <div className="container">
+        <Link href="/">
+          <Image
+            src="/logo.webp"
+            alt="Posto Menor Preço"
+            height={30}
+            width={150}
+          />
+        </Link>
+
+        <Separator />
+      </div>
+
+      <div className="container flex items-center justify-between py-4">
+        <nav className="flex items-center gap-4 sm:gap-8">
+          {NAV_ITEMS.map((item) => (
+            <NavItem {...item} key={item.label}></NavItem>
+          ))}
+        </nav>
+
+        <Button>Encontre seu posto</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
