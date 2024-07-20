f77d92840e57d34c330881600c2199d09753f2af
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-07-19 21:27:31 -0300

    chore: add initial setup from project

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   prefix: "",
   theme: {
     container: {
@@ -27,12 +27,6 @@ const config = {
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
-        },
-        blue: {
-          DEFAULT: "hsl(var(--blue))",
-        },
-        red: {
-          DEFAULT: "hsl(var(--red))",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
@@ -81,6 +75,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+} satisfies Config
 
-export default config;
+export default config
